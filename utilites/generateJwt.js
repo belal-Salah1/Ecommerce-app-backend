4673b2df1bb794d7bcf997d7aa0754fc1d7bc002
belal-Salah1 <belal.salah259@gmail.com>
2025-07-21 19:27:37 +0300

    make the token time more long

--- utilites/generateJwt.js
+++ utilites/generateJwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateJwt = (payload)=>{
-    const token = jwt.sign(payload , process.env.JWT_SECRTER_KEY, { expiresIn: '10m' });
+    const token = jwt.sign(payload , process.env.JWT_SECRTER_KEY, { expiresIn: '15d' });
     return token;
 }
 module.exports = generateJwt;

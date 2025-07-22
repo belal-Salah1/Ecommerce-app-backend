88c2c7a21e3cc8366bb755072aa75030d807d40a
belal-Salah1 <belal.salah259@gmail.com>
2025-07-22 16:02:21 +0300

    edit on api

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 
 });
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/products',productsRouter );
 app.use('/api/categories', categoriesRouter);   
@@ -29,7 +30,6 @@ app.use((err,req,res,next)=>{
         message: err.message
     });
 })
-app.use(cors());
 
 app.get('/healthcheck', (req, res) => {
   res.status(200).send('OK');
